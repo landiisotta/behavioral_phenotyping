142c5303f8b0f4aada5e84070eb75d8159c60dbd
Valerio Maggio <vmaggio@fbk.eu>
2019-07-15 12:10:13 +0100

    removed useless utils dependency (never used)

--- features.py
+++ features.py
@@ -4,7 +4,7 @@ import re
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from dataset import data_path
+from .dataset import data_path
 from sklearn.preprocessing import StandardScaler
 
 
